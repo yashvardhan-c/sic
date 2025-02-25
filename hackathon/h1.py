1d2108febf857e16935fa0358d49fb19a0069d19
yashvardhan-c <yashvardhanchandrashekar@gmail.com>
2025-02-25 15:53:01 +0530

    hackathon files updated

--- hackathon/h1.py
+++ hackathon/h1.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the retail dataset
+# Load the dataset
 df = pd.read_csv(r'E:\Python_Learning\yash\Hackathon_dataset.csv')
 
 df = df.head(20)
 
-# Assume the file contains columns 'SKU' and 'Stocks' (adjust names based on actual CSV structure)
+
 sku_stock_data = df.groupby('SKU')['Sales_Amount'].sum()
 
 # Create the pie chart
 plt.figure(figsize=(8, 8))
 plt.pie(sku_stock_data, labels=sku_stock_data.index, autopct='%1.1f%%', startangle=140)
 plt.title('SKU vs Sales amount')
-plt.axis('equal')  # Equal aspect ratio ensures that pie chart is drawn as a circle.
+plt.axis('equal')
 plt.show()
--- hackathon/h3.py
+++ hackathon/h3.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the retail dataset
+# Load the dataset
 df = pd.read_csv(r'E:\Python_Learning\yash\Hackathon_dataset.csv')
 
 df = df.head(20)
 
-# Assume the dataset contains 'Product ID' and 'Units Sold' columns
-# Let's sum the units sold for each product
+
 product_sales = df.groupby('Product ID')['Units Sold'].sum()
 
-# Plot the histogram (bar chart)
+
 plt.figure(figsize=(12, 6))
 product_sales.plot(kind='bar', color='lightgreen')
 
-# Adding titles and labels
+
 plt.title('Product ID vs Units Sold', fontsize=16)
 plt.xlabel('Product ID', fontsize=14)
 plt.ylabel('Total Units Sold', fontsize=14)
 
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
-plt.tight_layout()  # Adjust layout to avoid label clipping
+plt.xticks(rotation=45) 
+plt.tight_layout()  
 plt.show()
